256e8c6713d095ec1dadb40719a696927c666d82
yoryet <y.danoun@pharmatender.cl>
2024-11-17 21:51:40 -0300

    comentarios: adicion de codigo comentario para encriptar nuevas claves en bd de forma segura con BCryptPasswordEncoder

--- src/main/java/com/demo/recetas/RecetasApplication.java
+++ src/main/java/com/demo/recetas/RecetasApplication.java
@@ -6,8 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RecetasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RecetasApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RecetasApplication.class, args);
 
+        /*// Crear el encriptador BCrypt
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        // Generar hashes para las contraseñas
+		 
+        String password1 = "password1";
+        String encodedPassword1 = encoder.encode(password1);
+        System.out.println("Hash generado para usuario1: " + encodedPassword1);
+
+        String password2 = "password2";
+        String encodedPassword2 = encoder.encode(password2);
+    
+        System.out.println("Hash generado para usuario2: " + encodedPassword2);}*/
+    }
 }
